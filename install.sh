a9e6b410fab02a9c28fc0098049ff3ef7f1d5c04
ruineka <ruinairas1992@gmail.com>
2022-06-29 00:42:36 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "Installing HandyGCCS..."
-sudo pacman -Sy --noconfirm - < pkg_depends.list
+sudo apt -y install - < pkg_depends.list
 < pip_depends.list xargs python3 -m pip install
 echo "Enabling controller functionality. NEXT users will need to configure the Home button in steam."
 cp -v handycon.py /usr/local/bin/
